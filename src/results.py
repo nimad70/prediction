f1892b074dc4db2ac0a875972af5f31986ab71a8
nima daryabar <nima.daryabar@yahoo.com>
2019-07-15 18:43:21 +0430

    results.py: rows is extracted from xlsx file

--- src/results.py
+++ src/results.py
@@ -1,1 +1,20 @@
-import csv
+import xlrd
+
+# Giving location of the file
+file_location = ('doc/results.xlsx')
+
+# Opening the work book
+wb = xlrd.open_workbook(file_location)
+sheet = wb.sheet_by_index(0)
+
+# Extracting first cell
+""" data = sheet.cell_value(0, 0)
+print("first cell: ",data) """
+
+# Extracting number of rows
+"""print("rows: ", sheet.nrows)"""
+
+# Extracting number of columns
+"""print("columns: ", sheet.ncols)"""
+
+
