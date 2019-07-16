d3f6dcd57fd8919838c69dadc53fbf092f75f1f8
nima daryabar <nima.daryabar@yahoo.com>
2019-07-16 12:38:59 +0430

    results.py: data[] is created

--- src/results.py
+++ src/results.py
@@ -8,24 +8,34 @@ wb = xlrd.open_workbook(file_location)
 sheet = wb.sheet_by_index(0)
 
 # Extracting first cell
-""" data = sheet.cell_value(0, 0)
-print("first cell: ",data) """
+"""
+data = sheet.cell_value(0, 0)
+print("first cell: ",data)
+"""
 
 # Extracting number of rows
-"""print("rows: ", sheet.nrows)"""
+"""
+print("rows: ", sheet.nrows)
+"""
 
 # Extracting number of columns
-"""print("columns: ", sheet.ncols)"""
+"""
+print("columns: ", sheet.ncols)
+"""
 
-rows = []
+# list of scores list
 data = []
 
+# Making the list
 for row in range(sheet.nrows):
-    r = []
+    single_row = []
     for col in range(sheet.ncols):
-        r.append(sheet.cell_value(row, col))
-        print(r)
-    rows.append(r)
+        single_row.append(sheet.cell_value(row, col))
+        # print(single_row)
+    data.append(single_row)
 
-
-
+# Just for checking items in data[]
+"""
+for _ in data:
+    print(_)
+"""
