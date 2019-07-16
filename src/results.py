f90d98dcebe8f091323402d5d74014cb3045f103
nima daryabar <nima.daryabar@yahoo.com>
2019-07-16 12:13:19 +0430

    making a list of scores

--- src/results.py
+++ src/results.py
@@ -17,4 +17,15 @@ print("first cell: ",data) """
 # Extracting number of columns
 """print("columns: ", sheet.ncols)"""
 
+rows = []
+data = []
+
+for row in range(sheet.nrows):
+    r = []
+    for col in range(sheet.ncols):
+        r.append(sheet.cell_value(row, col))
+        print(r)
+    rows.append(r)
+
+
 
