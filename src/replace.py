d91a027acd7e70ca6ad5f40c4d5246e34ab8b548
nima daryabar <nima.daryabar@yahoo.com>
2019-07-21 16:39:59 +0430

    replace.py: some comments are added at the top of the code

--- src/replace.py
+++ src/replace.py
@@ -1,3 +1,9 @@
+# Author: Nima Daryabar
+"""
+    A list argument will be passed to refine_list() and values with 'absent'
+    will be replaced by 0 and new list with 0 values will be returend
+"""
+
 # Replacing 'absent' with 0
 # List with 0 instead of 'absent' will be returned
 def refine_list(sample_list):
