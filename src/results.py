6a182fe092e1536474d33da0b56f65baea2abe3e
nima daryabar <nima.daryabar@yahoo.com>
2019-07-21 16:33:08 +0430

    results.py: refine_list() is removed from code

--- src/results.py
+++ src/results.py
@@ -15,17 +15,7 @@ import xlrd
 from sklearn import tree
 
 
-# Replacing 'absent' with 0
-# List with 0 instead of 'absent' will be returned
-def refine_list(sample_list):
-    # Search through every list in the list 
-    # and replacing 'absent' with 0
-    for lst in sample_list:
-        for _, item in enumerate(lst):
-            # print(_, "  ", item)
-            if item == 'ABSENT':
-                lst[_] = 0
-    return sample_list
+
     
 
 # Calculate average for each list
