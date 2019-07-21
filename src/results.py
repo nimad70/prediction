ec497d7d433d52566897729c3e3b214d6f02f54e
nima daryabar <nima.daryabar@yahoo.com>
2019-07-21 16:45:22 +0430

    results.py: ielts_scores() is removed

--- src/results.py
+++ src/results.py
@@ -16,27 +16,7 @@ from sklearn import tree
 from replace import refine_list
 
 
-# Calculate average for each list
-def ielts_scores(ielts_list):
-    # Iterate through the list to calculate average for each item(list)
-    for _, lst in enumerate(ielts_list):
-        sum = 0
-        # print(_ , lst)
-        # Adding items in the list
-        for item in lst:
-            sum += item
-            # print(sum)
-        # print("lenght of list is: ", len(lst))
-        # Calculate Score by dividing sum into lenght of list(i.e 4)
-        score = sum / len(lst)
-        lst.append(score)
-        # Just for check first 5 lists
-        # print(score)
-        # print(lst)
-        # print()
-        # if(_ == 5):
-            # break
-    return ielts_list
+
 
 
 
